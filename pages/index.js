e5b788cf521838581d761a0adf5a3c37cc595253
bagus tri harjanto <bagustri15@gmail.com>
2021-06-04 17:27:33 +0700

    add next/image

--- pages/index.js
+++ pages/index.js
@@ -43,6 +43,8 @@
 // =================================================================
 
 import { useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
 import axiosApiIntances from "../utils/axios";
 import Layout from "../components/Layout";
 import Navbar from "../components/module/Navbar";
@@ -70,17 +72,36 @@ export async function getServerSideProps(context) {
 }
 
 export default function Home(props) {
+  const router = useRouter();
   // console.log(props);
   const [users, setUsers] = useState(props.users);
+
+  const handleProfile = (id) => {
+    router.push(`/profile/${id}`);
+  };
 
   return (
     <Layout title="Home">
       <Navbar />
-      <h1 className={styles.titleHead}>Home Page !</h1>
-      <h2>{process.env.APP_NAME}</h2>
+      <br />
+      {/* <img src="/vercel.svg" alt="My Image" className="logo" /> */}
+      <div>
+        <Image
+          src="/profile/vercel.svg"
+          alt="Picture of the author"
+          width=""
+          height=""
+        />
+      </div>
+      {/* <h1 className={styles.titleHead}>Home Page !</h1> */}
+      {/* <h2>{process.env.APP_NAME}</h2> */}
       {users.map((item, index) => (
-        <div className="d-grid gap-2" key={index}>
-          <button className="btn btn-primary" type="button">
+        <div className="d-grid gap-2 my-2" key={index}>
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => handleProfile(item.id)}
+          >
             {item.name}
           </button>
         </div>
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,8 +1,10 @@
 import Layout from "../../components/Layout";
+import Navbar from "../../components/module/Navbar";
 
 export default function Profile() {
   return (
     <Layout title="Profile">
+      <Navbar />
       <h1>Profile Page !</h1>
     </Layout>
   );
